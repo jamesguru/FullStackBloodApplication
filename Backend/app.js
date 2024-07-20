e16482a313e2c1f9bf597c71b0b594afb6ed4c8a
unknown <jameskagunga2022@gmail.com>
2024-07-20 12:34:15 +0300

    added authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-
-
+const authRoute = require("./routes/auth")
 module.exports=app;
+
+//CORS
+app.use(cors());
+
+// JSON
+app.use(express.json());
+
+// ROUTES
+app.use("/api/v1/auth",authRoute)
